b737f8db01113548b938d369371c21aa8c5f04fa
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-08-17 18:32:00 +0200

    Add deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/weather-app-vuejs/' : '/';
+  publicPath: process.env.NODE_ENV === 'production' ? '/weather-app-vuejs/' : '/'
 }
